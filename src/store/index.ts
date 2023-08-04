d64e670faa4898ad0b8b1a07177e4eebbc5704c3
Rinat <r.z.zaripov@gmail.com>
2023-08-04 16:08:09 +0300

    подключил к хранилищу thunk c экземпляром axios

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,4 +1,14 @@
 import {configureStore} from '@reduxjs/toolkit';
 import { reducer } from './reducer';
+import { createApi } from '../services/api';
 
-export const store = configureStore({reducer});
+const axiosApi = createApi();
+export const store = configureStore({
+  reducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      thunk: {
+        extraArgument: axiosApi,
+      },
+    }),
+});
